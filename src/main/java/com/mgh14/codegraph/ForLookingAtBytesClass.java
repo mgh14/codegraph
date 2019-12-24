8799062e857df8672b805655c687004adeef6eeb
mgh14 <chasing314@gmail.com>
2019-12-24 00:28:37 -0700

    Suppress warnings for example looking-at-bytes class

--- src/main/java/com/mgh14/codegraph/ForLookingAtBytesClass.java
+++ src/main/java/com/mgh14/codegraph/ForLookingAtBytesClass.java
@@ -6,32 +6,39 @@ import java.util.ArrayList;
 /** TODO: Document */
 public class ForLookingAtBytesClass implements Serializable {
 
+  @SuppressWarnings("unused")
   private static final String STATIC_STRING = "Static string";
+  @SuppressWarnings("unused")
   private static final Object obj;
 
   static {
     obj = new ArrayList<>();
   }
 
+  @SuppressWarnings({"unused", "WeakerAccess"})
   public ForLookingAtBytesClass(int x) {
     final String concat = "y";
     final String concat2 = "y" + getAString();
     int t = 5;
   }
 
+  @SuppressWarnings("unused")
   public ForLookingAtBytesClass() {}
 
+  @SuppressWarnings("WeakerAccess")
   String getAString() {
     return "AString";
   }
 
+  @SuppressWarnings("unused")
   public static ForLookingAtBytesClass createObject() {
-    final ForLookingAtBytesClass flabcClass = new ForLookingAtBytesClass(10230);
-    final String aString = flabcClass.getAString();
+    final ForLookingAtBytesClass forLookingAtBytesClass = new ForLookingAtBytesClass(10230);
+    final String aString = forLookingAtBytesClass.getAString();
 
-    return flabcClass;
+    return forLookingAtBytesClass;
   }
 
+  @SuppressWarnings("unused")
   public String getSecondString() {
     return "secondString";
   }
